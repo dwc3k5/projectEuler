4d329f6fc95177c41005abccf770ae680b9e9d9b
dwc3k5 <dwc1608@gmail.com>
2018-04-07 16:14:08 -0500

    core of 12 done

--- problemTwelve.js
+++ problemTwelve.js
@@ -1,30 +1,31 @@
 console.log("working on 10");
-//this takes waaaay too long to process. Need to find a more agile process
-var primeArray = [2];
-var total = 2;
-var i = 1;
+var natNumArray = [];
+var triNum = 0;
+counter = 0;
 
+//loop scans factors of i
+//each factor added to natNumArray
 
 function test(){
-  console.log("running test");
-  do{
-    //all even numbers are divisible by 2, therefore, skip all evens
-    i += 2;
-    //scan through array of prime numbers
-    for(j = 0; j < primeArray.length; j++){
-    //if the current iteration is divisible by a prime, skip it
-      if (i % primeArray[j] === 0){
-        break;
-      }else if(i % primeArray[j] !== 0 && j === primeArray.length-1){
-        total +=i;
-        primeArray.push(i);
-
-      }
+do{
+  counter++;
+  triNum += counter;
+  console.log("TriNum is " + triNum);
+  for(i=1; i <= triNum; i++){
+    if(triNum % i === 0){
+      natNumArray.push(i);
     }
-    //defines which prime number I'm looking for
-  }while(i <= 2000000);
-  if(i >= 1000){
-    console.log("your total is "+ total);
   }
+  if(natNumArray.length > 5){
+    return console.log(triNum);
+  }else{
+    natNumArray = [];
+  }
+  if(counter >= 20){
+    console.log("ceiling");
+  }
+
+
+}while(counter < 20);
 }
 test();
