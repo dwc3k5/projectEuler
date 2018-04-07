70b34c5ecc672f867f9d7c8a90e29106bb77da7c
dwc3k5 <dwc1608@gmail.com>
2018-04-07 15:56:06 -0500

    working on project 12

--- problemTwelve.js
+++ problemTwelve.js
@@ -1,12 +1,12 @@
-console.log("working on");
-//this one I had to reverse engineer and put "*" next to the sections I'm still struggling with. Decked the code out with notes to help understanding
-
-var answer ;
-var primeArray =[2];
+console.log("working on 10");
+//this takes waaaay too long to process. Need to find a more agile process
+var primeArray = [2];
+var total = 2;
 var i = 1;
 
 
 function test(){
+  console.log("running test");
   do{
     //all even numbers are divisible by 2, therefore, skip all evens
     i += 2;
@@ -16,13 +16,15 @@ function test(){
       if (i % primeArray[j] === 0){
         break;
       }else if(i % primeArray[j] !== 0 && j === primeArray.length-1){
+        total +=i;
         primeArray.push(i);
-      }
-      if(primeArray.length === 10001){
-        console.log(primeArray[10000]);
+
       }
     }
     //defines which prime number I'm looking for
-  }while(primeArray.length < 10001);
+  }while(i <= 2000000);
+  if(i >= 1000){
+    console.log("your total is "+ total);
+  }
 }
 test();
