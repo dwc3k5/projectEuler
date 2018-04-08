64ba869e298f4bf6f66ba836e1ffc466db7ec929
dwc3k5 <dwc1608@gmail.com>
2018-04-08 13:22:00 -0500

    coding problem 15 solution

--- problemFifteen.js
+++ problemFifteen.js
@@ -1,16 +1,23 @@
 console.log("connected");
 
 //for lattice size
-var cap = 3;
+var cap = 20;
+//eventual answer
+var choices  ;
 //tracking where program is in process
-var leftCount = 0;
-var num = [0];
-var choices = 2;
-
+var currentRow = [1];
+var prevRow = [1,2];
 
 function test(){
   for(i = 0; i < cap; i++){
-    choices += num[i];
+    for(j = 0; j < prevRow.length; j++){
+      
+    }
+
+    if(currentRow.length === prevRow.length){
+      currentRow.length += currentRow.length;
+      prevRow = currentRow;
+    }
   }
 }
 test();
