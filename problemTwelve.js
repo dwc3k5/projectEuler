4a5996fc2192b7d9b8e4c3d59f54dcfdd89932d9
dwc3k5 <dwc1608@gmail.com>
2018-04-08 11:36:18 -0500

    problem 15

--- problemTwelve.js
+++ problemTwelve.js
@@ -1,31 +1,41 @@
 console.log("working on 10");
-var natNumArray = [];
+var divisorCount = 0;
 var triNum = 0;
 counter = 0;
 
-//loop scans factors of i
-//each factor added to natNumArray
-
+//still working on this one looked up the number and
 function test(){
-do{
-  counter++;
-  triNum += counter;
-  console.log("TriNum is " + triNum);
-  for(i=1; i <= triNum; i++){
-    if(triNum % i === 0){
-      natNumArray.push(i);
+
+  while(divisorCount <500){
+    counter++;
+    triNum += counter;
+    sqrtTriNum = Math.sqrt(triNum);
+    for(i = 1; i < sqrtTriNum; i++){
+      if(sqrtTriNum % i === 0){
+        divisorCount += 2;
+      }
     }
-  }
-  if(natNumArray.length > 5){
-    return console.log(triNum);
-  }else{
-    natNumArray = [];
-  }
-  if(counter >= 20){
-    console.log("ceiling");
-  }
+    if(divisorCount === 500){
+      console.log(triNum);
+    }
 
 
-}while(counter < 20);
+  }
+// do{
+//   counter++;
+//   triNum += counter;
+//   for(i=1; i <= triNum; i++){
+//     if(triNum % i === 0){
+//       natNumArray.push(i);
+//     }
+//   }
+//   if(natNumArray.length > 10){
+//     return console.log(triNum);
+//   }else{
+//     natNumArray = [];
+//   }
+//
+// }while(natNumArray.length < 10);
 }
+
 test();
