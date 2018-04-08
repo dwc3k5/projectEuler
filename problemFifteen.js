52736461536455b0304ab681f08085f0bbf6f173
dwc3k5 <dwc1608@gmail.com>
2018-04-08 14:48:47 -0500

    problem 15 solved and coded

--- problemFifteen.js
+++ problemFifteen.js
@@ -1,23 +1,33 @@
 console.log("connected");
 
 //for lattice size
-var cap = 20;
+var cap = 18;
 //eventual answer
 var choices  ;
 //tracking where program is in process
-var currentRow = [1];
-var prevRow = [1,2];
+var currentRow = [1,4];
+var prevRow = [1,3,6];
+var counter = 4;
 
 function test(){
   for(i = 0; i < cap; i++){
-    for(j = 0; j < prevRow.length; j++){
-      
+    //might be <=
+    for(j = 1; j < prevRow.length; j++){
+      if(currentRow.length === prevRow.length){
+        // console.log(currentRow);
+        // console.log(prevRow);
+        //not sure if this code is accurate
+        currentRow.push(currentRow[j]*2);
+        choices = currentRow[j+1];
+        prevRow = currentRow;
+        counter ++;
+        currentRow = [1,counter];
+        break;
+      }else{
+        currentRow.push((currentRow[j]+prevRow[j+1]));
+      }
     }
-
-    if(currentRow.length === prevRow.length){
-      currentRow.length += currentRow.length;
-      prevRow = currentRow;
-    }
+    console.log("number of choices = "+choices);
   }
 }
 test();
